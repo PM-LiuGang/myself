303f7c8a124fd2821b3abd23f30532ffe7a4cb85
pmliugang <31712415@qq.coom>
2018-08-26 00:32:51 +0800

    add

--- python_数据分析与数据化运营/基于AdaBoos的营销分析/基于AdaBoos的营销分析.py
+++ python_数据分析与数据化运营/基于AdaBoos的营销分析/基于AdaBoos的营销分析.py
@@ -25,6 +25,9 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder#将分类变量和顺序变量转换为二值化变量
+import warnings
+
+warnings.filterwarnings('ignore')
 '''
 stratifiedkfold,cross val score前者用来将数据分为训练集和测试集，后者用来做交叉检验，这里选择的stra能够
 有效的结合分类样本标签数据做数据集分割，而不是完全的随机选择和分割，这种方法在应对分类样本不均匀时尤为有效
@@ -93,7 +96,7 @@ def type_con(df):
                 'act_level':'int32',
                 'sex':'int32',
                 'region':'int32'}
-    for var,type in var_list.iteritems():
+    for var,type in var_list.items():
         df[var] = df[var].astype(type)
     print('Data Types')
     print(df.dtypes)
@@ -143,4 +146,102 @@ def symbol_con(df,enc_object=None,train=True):
         df_con_new = enc_object.transform(df_con).toarray()
         new_matrix = np.hstack((df_con_new,df_org))
         return new_matrix
-                
+
+def get_best_model(x,y):
+    '''
+    结合交叉检验得到不同参数下的分类模型结果
+    param x 输入x（特征变量）
+    param y 输入y（目标变量）
+    return 特征选择模型对象
+    '''
+    #SelectPercentile 按照指定方法选择特定数量或比例的特征变量
+    #f_classif 计算数据集的方差P值
+    transform = SelectPercentile(f_classif,percentile=50)
+    model_adaboost = AdaBoostClassifier()
+    #管道对象 语法 Pipline(steps=元组组成的列表，元组名称，模型对象)
+    model_pipe = Pipeline(steps=[('ANOAV',transform),('model_adaboost',model_adaboost)])
+    cv = StratifiedKFold(5)
+    n_estimators = [20,50,80,100]
+    score_methods = ['accuracy','f1','precision','recall','roc_auc']
+    mean_list = list()
+    std_list = list()
+    for parameter in n_estimators:
+        t1 = time.time()
+        score_list = list()
+        print('Set Parameters: %s' % parameter)
+        for score_method in score_methods:
+            '''
+            步骤名称 + __ +参数对象的方法设置
+            这里的步骤名称是在建立管道时定义的字符串名称
+            __ 表示连接对应步骤对象的参数 后面接具体参数的名称
+            model_adaboost__n_estimators=parameter的意思是：
+            model_adaboost的n_estimators参数的值为parameter，parameter为从外层循环读取的值
+            '''
+            model_pipe.set_params(model_adaboost__n_estimators=parameter)#通过 管道 设置分类模型参数
+            score_tmp = cross_val_score(model_pipe,x,y,scoring=score_method,cv=cv)
+            score_list.append(score_tmp)
+        score_matrix = pd.DataFrame(np.array(score_list),index=score_methods)
+        score_mean = score_matrix.mean(axis=1).rename('mean')
+        score_std = score_matrix.std(axis=1).rename('std')
+        score_pd = pd.concat([score_matrix,score_mean,score_std],axis=1)
+        mean_list.append(score_mean)
+        std_list.append(score_std)
+        print(score_pd.round(2))
+        print('-' * 60)
+        t2 = time.time()
+        tt = t2 - t1
+        print('time: %s' % str(tt))
+    mean_matrix = np.array(mean_list).T
+    std_matrix = np.array(std_list).T
+    mean_pd = pd.DataFrame(mean_matrix,index=score_methods,columns=n_estimators)
+    std_pd = pd.DataFrame(std_matrix,index=score_methods,columns=n_estimators)
+    print('Mean values for each parameter')
+    print(mean_pd)
+    print('Std values for each parameter:')
+    print(std_pd)
+    print('-' * 60)
+    return transform
+
+#程序应用
+raw_data = pd.read_excel(r'D:\python\python_数据分析与数据化运营\chapter5\order.xlsx',sheet_name=0)
+x = raw_data.drop('response',axis=1)
+y = raw_data['response']
+
+set_summary(raw_data)
+na_summary(raw_data)
+label_summary(raw_data)
+
+x_t1 = na_replace(x)
+x_t2 = type_con(x_t1)
+
+na_summary(x_t1)
+#label_summary(x_t2) x_t2是没有response
+#将分类和顺序数据转换为标志
+x_new,enc = symbol_con(x_t2,enc_object=None,train=True)
+transform = get_best_model(x_new,y)
+transform.fit(x_new,y)
+x_final = transform.transform(x_new)
+final_model = AdaBoostClassifier(n_estimators=100)
+final_model.fit(x_final,y)
+#新数据集做预测
+new_data = pd.read_excel(r'D:\python\python_数据分析与数据化运营\chapter5\order.xlsx',sheet_name=1)
+final_reponse = new_data['final_response']
+new_data = new_data.drop('final_response',axis=1)
+set_summary(new_data)
+na_summary(new_data)
+new_x_t1 = na_replace(new_data)
+new_x_t2 = type_con(new_x_t1)
+new_x_t3 = symbol_con(new_x_t2,enc_object=enc,train=False)
+new_x_final = transform.transform(new_x_t3)
+#预测
+predict_labels = pd.DataFrame(final_model.predict(new_x_final),columns=['lables'])
+predict_labels_pro = pd.DataFrame(final_model.predict_proba(new_x_final),columns=['pro1','pro2'])
+predict_pd = pd.concat((new_data,predict_labels,predict_labels_pro),axis=1)
+print('Predict Info')
+print(predict_pd.head(2))
+print('-' * 60)
+#将结果写入到excel里
+writer = pd.ExcelWriter('D:\python\python_数据分析与数据化运营\chapter5\order_predict_result_myself.xlsx')
+predict_pd.to_excel(writer,'Sheet1')
+writer.save()
+
