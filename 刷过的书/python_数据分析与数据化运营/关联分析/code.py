2af0a1a4027d8dfcf66d6e1cf17aa1c5c87c0d73
pmliugang <31712415@qq.coom>
2018-09-27 07:33:00 +0800

    review

--- 刷过的书/python_数据分析与数据化运营/关联分析/code.py
+++ 刷过的书/python_数据分析与数据化运营/关联分析/code.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 14 22:21:18 2018
-
+描述：4-4-6 python关联分析
 @author: 刘刚
 """
-
-####################################################################
-# 4.4 关联分析
-# 导入库
 import sys
-import os 
-os.chdir(r'd:\python\python_数据分析与数据化运营\chapter4')
 import pandas as pd
+import apriori # 自定义
 from graphviz import Digraph
-import apriori
 
-# 定义数据文件
 fileName = 'association.txt'
 
-# 通过调用自定义的apriori做关联分析
 minS = 0.1  # 定义最小支持度阀值
 minC = 0.38  # 定义最小置信度阀值
 dataSet = apriori.createData(fileName)  # 获取格式化的数据集
 L, suppData = apriori.apriori(dataSet, minSupport=minS)  # 计算得到满足最小支持度的规则
-rules = apriori.generateRules(fileName, L, suppData, minConf=minC)  # 计算满足最小置信度的规则
-
+rules = apriori.generateRules(fileName, 
+                              L, 
+                              suppData, 
+                              minConf=minC)  # 计算满足最小置信度的规则
 # 关联结果报表评估
 model_summary = 'data record: {1} \nassociation rules count: {0}'  # 展示数据集记录数和满足阀值定义的规则数量
 print (model_summary.format(len(rules), len(dataSet)))  # 使用str.format做格式化输出
-df = pd.DataFrame(rules, columns=['item1', 'itme2', 'instance', 'support', 'confidence', 'lift'])  # 创建频繁规则数据框
+df = pd.DataFrame(rules, 
+                  columns=['item1', 
+                           'itme2', 
+                           'instance', 
+                           'support', 
+                           'confidence', 
+                           'lift'])  # 创建频繁规则数据框
 df_lift = df[df['lift'] > 1.0]  # 只选择提升度>1的规则
 print (df_lift.sort('instance', ascending=False))  # 打印排序后的数据框
 
