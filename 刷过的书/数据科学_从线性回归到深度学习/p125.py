19a0f9c924222d24a10a2d046b166fdacba386ba
PM_LiuGang <317121415@qq.com>
2019-02-27 21:46:44 +0800

    review

--- 刷过的书/数据科学_从线性回归到深度学习/p125.py
+++ 刷过的书/数据科学_从线性回归到深度学习/p125.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 创建时间：Tue Feb  5 15:48:25 2019
-描述：此脚本用户展示ROC曲线和AUC
+描述：展示ROC曲线和AUC
 作者: PM.LiuGang
-Review:
+Review:20190220
 遗留：
 """
 
-#import os
-#import sys
 import matplotlib.pyplot as plt
-#import numpy as np
 import pandas as pd
 
 from sklearn import metrics
@@ -23,6 +20,15 @@ plt.rcParams["font.sans-serif"] = ["SimHei"]
 def transLabel(data):
     '''
     将文字变量转换为数字变量
+    
+    Parameters
+    ----------
+    data : pd.DataFrame
+    
+    Return
+    ------
+    data : pd.DataFrame
+        add columns 'label_code', [0,1]
     '''
     data['label_code'] = pd.Categorical(data.label).codes
     return data
@@ -31,6 +37,21 @@ def transLabel(data):
 def trainModel(data, features, labels):
     '''
     搭建逻辑回归模型，并训练模型
+    
+    Parameters
+    ----------
+    data : pd.DataFrame
+        will be fitted data
+        
+    features : list[str]
+        trainned data features
+        
+    label : list[str]
+        trainned data labels
+        
+    Returns
+    -------
+    model : fitted-LogisticRegression-model
     '''
     model = LogisticRegression()
     model.fit(data[features], data[labels])
@@ -38,6 +59,18 @@ def trainModel(data, features, labels):
 
 
 def readData(path):
+    '''
+    用pd读取数据
+    
+    Parameters
+    ---------
+    path : str
+        file name, file name with abs path
+        
+    Returns
+    -------
+    data[cols] : 原data中的几列
+    '''
     data = pd.read_csv(path, encoding='utf-8')
     cols = ['age', 'education_num', 'capital_gain', 'capital_loss',
             'hours_per_week', 'label']
@@ -46,13 +79,25 @@ def readData(path):
 
 def visualizeRoc(fpr, tpr, auc):
     '''
-
+    可视化ROC曲线
+    
+    Parameters
+    ----------
+    fpr : str
+        FP + FN / FP + FN + TP + TN
+    tpr : str
+        TP + TN / FP + FN + TP + TN
+    auc : str , [0,1]
+        AUC的面积
+    Returns
+    -------
+    matlibplot.pyplot.Axes
     '''
     fig = plt.figure(figsize=(6,6), dpi=80)
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(111)
     ax.set_title('%s' % 'ROC曲线')
-    ax.set_xlabel('False positive rate')
-    ax.set_ylabel('True positive rate')
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
     ax.plot([0, 1], [0, 1], 'r--')
     ax.set_xlim([0, 1])
     ax.set_ylim([0, 1])
@@ -60,25 +105,33 @@ def visualizeRoc(fpr, tpr, auc):
             tpr, 
             'k', 
             label='%s: %s = %0.2f' % ('ROC曲线', '曲线下面积(AUC)', auc))
-    ax.fill_between(fpr, tpr, color='grey', alpha=0.6)
+    ax.fill_between(fpr, tpr, color='grey', alpha=0.6) # ?
     plt.legend(shadow=True)
     plt.show()
 
 
 def logitRegression(data):
     '''
-
+    逻辑回归主程序
+    
+    Parameters
+    ----------
+    data : pd.DataFrame
+    
+    Returns
+    -------
+    
     '''
     data = transLabel(data)
-    features = ['age', 'education_num', 'capital_gain', 'capital_loss',
-                'hours_per_week']
+    features = ['age', 'education_num', 'capital_gain', 
+                'capital_loss','hours_per_week']
     labels = 'label_code'
     trainSet, testSet = train_test_split(data,
                                          test_size=.2,
                                          random_state=2310)
     model = trainModel(trainSet, features, labels)
     preds = model.predict_proba(testSet[features])[:, 1]
-    fpr, tpr, _ = metrics.roc_curve(testSet[labels], preds)
+    fpr, tpr, _ = metrics.roc_curve(testSet[labels], preds) # thresholds
     auc = metrics.auc(fpr, tpr)
     visualizeRoc(fpr, tpr, auc)
 
