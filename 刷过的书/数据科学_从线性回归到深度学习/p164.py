9fb663594f206afe03e22f0a010698c874d7a44c
liug <gang.liu@hlytec.com>
2019-03-01 17:14:31 +0800

    review

--- 刷过的书/数据科学_从线性回归到深度学习/p164.py
+++ 刷过的书/数据科学_从线性回归到深度学习/p164.py
@@ -3,7 +3,7 @@
 创建时间：Thu Feb  7 16:43:25 2019
 描述：此脚本用于如何处理定性变量
 作者: PM.LiuGang
-Review:
+Review:190301
 遗留：
 """
 
@@ -20,15 +20,28 @@ from statsmodels.graphics.mosaicplot import mosaic
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 
 
-def transLabel(data):
+def transLabel(data, columnsName):
     '''
     将文字变量转化为数字变量
     '''
-    data['label_code'] = pd.Categorical(data.label).codes
+    data[columnsName + '_code'] = pd.Categorical(data[columnsName]).codes 
     return data
 
 
 def readData(path):
+    '''
+    读取指定文件的数据
+    
+    Parameters
+    ----------
+    path : str
+        file name(path)
+        
+    Returns:
+    --------
+    data[col] : pd.DataFrame
+        data
+    '''
     data = pd.read_csv(path)
     cols = ["workclass",
             "sex",
@@ -37,7 +50,7 @@ def readData(path):
             "capital_gain",
             "capital_loss",
             "hours_per_week",
-            "label"]
+            "label"] # 列名根据需求进行修改
     return data[cols]
 
 
@@ -45,7 +58,6 @@ def trainModel2(data):
     '''
     加入workclass变量，搭建逻辑回归模型，并训练模型
     '''
-    # 定义workclass的类别顺序，数组里的第一个值为基准类别
     l = [" ?",
          " Never-worked",
          " Without-pay",
@@ -55,13 +67,14 @@ def trainModel2(data):
          " Federal-gov",
          " Local-gov",
          " Self-emp-inc"]
-    # 定义各个类别对应的虚拟变量
-    contrast = np.eye(9, 6, k=-3)
-    # 为每个虚拟变量命名
-    contrast_mat = ContrastMatrix(contrast, l[3:])  # 公式中使用
+    contrast = np.eye(9, 6, k=-3) # 对角单位矩阵
+    contrast_mat = ContrastMatrix(contrast, l[3:]) # 对比矩阵
     formula = """label_code ~ C(workclass, contrast_mat, levels=l)
-        + C(sex) + education_num + capital_gain
-        + capital_loss + hours_per_week"""
+        + C(sex) 
+        + education_num 
+        + capital_gain
+        + capital_loss 
+        + hours_per_week""" # C ? 
     model = sm.Logit.from_formula(formula, data=data)
     re = model.fit()
     return re
@@ -69,13 +82,16 @@ def trainModel2(data):
 
 def trainModel3(data):
     '''
-
+    加入性别变量，搭建逻辑回归模型，并训练模型
     '''
-    l = [' Male', ' Female']  # 公式中使用
+    l = [' Male', ' Female']
     contrast = [[-0.33], [0.67]]
-    contrast_mat = ContrastMatrix(contrast, ['Ridit(sex)'])  # 公式中使用
-    formula = """label_code ~ C(sex, contrast_mat, levels=l) \
-        + education_num + capital_gain + capital_loss + hours_per_week"""
+    contrast_mat = ContrastMatrix(contrast, ['Ridit(sex)'])
+    formula = """label_code ~ C(sex, contrast_mat, levels=l) 
+        + education_num 
+        + capital_gain 
+        + capital_loss 
+        + hours_per_week"""
     model = sm.Logit.from_formula(formula, data=data)
     re = model.fit()
     return re
@@ -83,10 +99,14 @@ def trainModel3(data):
 
 def trainModel(data):
     '''
-
+    加入性别变量，搭建逻辑回归模型，并训练模型
     '''
-    formula = "label_code ~ C(sex) + education_num \
-        + capital_gain + capital_loss + hours_per_week"
+    formula = '''
+        label_code ~ C(sex) 
+        + education_num 
+        + capital_gain 
+        + capital_loss 
+        + hours_per_week'''
     model = sm.Logit.from_formula(formula, data=data)
     re = model.fit()
     return re
@@ -94,7 +114,7 @@ def trainModel(data):
 
 def baseModel(data):
     '''
-
+    基本逻辑回归模型
     '''
     formula = "label_code ~ education_num + capital_gain \
         + capital_loss + hours_per_week"
@@ -105,7 +125,21 @@ def baseModel(data):
 
 def makePrediction(re, testSet, alpha=0.5):
     '''
-
+    对验证集进行预测，然后对预测结果进行离散分类
+    
+    Parameters
+    ----------
+    re : model
+        已训练好的模型
+    testSet : pd.DataFrame
+        验证集
+    alpha : float
+        阈值，如果大于阈值置1，否则置0
+        
+    Returns
+    -------
+    data pd.DataFrame
+        添加预测概率列和分类标签
     '''
     pd.options.mode.chained_assignment = None
     data = testSet.copy()
@@ -117,8 +151,20 @@ def makePrediction(re, testSet, alpha=0.5):
 
 def evaluation(newRe, baseRe):
     '''
-
+    评估新模型和基础模型的效果（泛化能力）
+    
+    Parameters
+    ----------
+    newRe : model
+        训练好的带C模型
+    baseRe : model
+        训练好的基础模型
+    
+    Returns
+    -------
+    matplotlib.pyplot.Axes
     '''
+    # 加入性别后的模型
     fpr, tpr, _ = metrics.roc_curve(newRe['label_code'],
                                     newRe['prob'])
     auc = metrics.auc(fpr, tpr)
@@ -130,9 +176,9 @@ def evaluation(newRe, baseRe):
     ax.plot([0, 1], [0, 1], 'r--')
     ax.set_xlim([0, 1])
     ax.set_ylim([0, 1])
-    ax.plot(fpr, tpr, 'k', label='%s;%s = %0.2f' %
-            ('加入性别后的ROC曲线', '曲线下面积（AUC）', auc))
-
+    ax.plot(fpr, tpr, 'k', 
+            label='%s;%s = %0.2f' % ('加入性别后的ROC曲线', '曲线下面积（AUC）', auc))
+    # 基础模型
     fpr, tpr, _ = metrics.roc_curve(baseRe['label_code'],
                                     baseRe['prob'])
     auc = metrics.auc(fpr, tpr)
@@ -160,7 +206,7 @@ def logitRegression(data):
     '''
 
     '''
-    data = transLabel(data)
+    data = transLabel(data,'label')
     analyseData(data)
     trainSet, testSet = train_test_split(data,
                                          test_size=0.2,
