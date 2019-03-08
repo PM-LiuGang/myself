32b80423fb7181470c7ac372cce1d7073282b6ce
liug <gang.liu@hlytec.com>
2019-03-08 15:36:28 +0800

    review

--- cmPlot.py
+++ cmPlot.py
@@ -4,22 +4,36 @@
 描述:分类可视模块
 作者:PM.liugang
 """
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
 
 def cmPlot(yTrue, yPred):
     '''
-    param yTrue
-    param yPred
-    return 可视化混淆矩阵
+    可视化混淆矩阵
+    
+    Parameters
+    ----------
+    yTrue : np.array or pd.DataFrame or pd.Series
+        原始数据集中的分类标签
+    yPred : np.array or pd.DataFrame or pd.Series
+        测试数据集中的预测分类标签
+    
+    Returns 
+    -------
+    可视化混淆矩阵
     '''
     cm = confusion_matrix(yTrue, yPred)  # 生成混淆矩阵
-    plt.matshow(cm, cmap=plt.cm.Greens)  # 画混淆矩阵图，配色风格使用cm.Greens
-    plt.colorbar()  # 颜色标签
-    for x in range(len(cm)):  # 数据标签
-        for y in range(len(cm)):
+    plt.matshow(cm, cmap=plt.cm.Greens)  # 配色风格使用cm.Greens
+    plt.colorbar()  # 颜色柱标签
+    for x in range(len(cm)):
+        for y in range(len(cm)): # 混淆矩阵都是方阵
             plt.annotate(cm[x, y],
                          xy=(x, y),
                          horizontalalignment='center',
                          verticalalignment='center')
-    plt.ylabel('True label')  # 坐标轴标签
-    plt.xlabel('Predicted label')  # 坐标轴标签
-    return plt
+    plt.ylabel('True Label')  # 坐标轴标签
+    plt.xlabel('Predicted Label')  # 坐标轴标签
+#    return plt 
+    plt.show()
+    
--- 刷过的书/数据科学_从线性回归到深度学习/linear_stat.py
+++ 刷过的书/数据科学_从线性回归到深度学习/linear_stat.py
@@ -1,81 +1,79 @@
 # -*- coding: UTF-8 -*-
 """
 此脚本用于展示如何使用statsmodels搭建线性回归模型
+review:190307
 """
 
 
 # 保证脚本与Python3兼容
-from __future__ import print_function
 
-import os
-import sys
-
-import numpy as np
+#import sys
+#import numpy as np
 import statsmodels.api as sm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+
+plt.rcParams['font.sans-serif']=['SimHei']
 
 def modelSummary(re):
     """
     分析线性回归模型的统计性质
+    
+    Parameters
+    ----------
+    re : model
+        fitted linear model
     """
-    # 整体统计分析结果
     print(re.summary())
-    # 在Windows下运行此脚本需确保Windows下的命令提示符(cmd)能显示中文
-    # 用f test检测x对应的系数a是否显著
     print("检验假设x的系数等于0：")
     print(re.f_test("x=0"))
-    # 用f test检测常量b是否显著
-    print("检测假设const的系数等于0：")
+#    print('{:*^80}'.format('分割线'))
+    print("检测假设const的系数等于0：") # 用f test检测常量b是否显著
     print(re.f_test("const=0"))
-    # 用f test检测a=1, b=0同时成立的显著性
+#    print('{:*^80}'.format('分割线'))
     print("检测假设x的系数等于1和const的系数等于0同时成立：")
     print(re.f_test(["x=1", "const=0"]))
+    print('{:*^80}'.format('分割线'))
 
-
-def visualizeModel(re, data, features, labels):
+def visualizeModel(re, data, features, labels, title=None):
     """
-    模型可视化
+    模型参数和效果可视化
+    
+    Parameters
+    ----------
+    re : model
+        fitted linear model
+    data : pd.DataFrame
+        datasets
+    features : str or [str,str,str....]
+        data features
+    labels : pd.Series or np.array([])
+        data labels
+        
+    Returns
+    -------
+    matplotlib.pyplot.Axes
+        
     """
-    # 计算预测结果的标准差，预测下界，预测上界
-    prstd, preLow, preUp = wls_prediction_std(re, alpha=0.05)
-    # 为在Matplotlib中显示中文，设置特殊字体
-    plt.rcParams['font.sans-serif']=['SimHei']
-    # 创建一个图形框
+    prstd, preLow, preUp = wls_prediction_std(re, alpha=0.05)   
     fig = plt.figure(figsize=(6, 6), dpi=80)
-    # 在图形框里只画一幅图
     ax = fig.add_subplot(111)
-    # 在Matplotlib中显示中文，需要使用unicode
-    # 在Python3中，str不需要decode
-    if sys.version_info[0] == 3:
-        ax.set_title(u'%s' % "线性回归统计分析示例")
-    else:
-        ax.set_title(u'%s' % "线性回归统计分析示例".decode("utf-8"))
+    ax.set_title('线性回归统计分析实例%s' % title )
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
-    # 画点图，用蓝色圆点表示原始数据
-    # 在Python3中，str不需要decode
-    if sys.version_info[0] == 3:
-        ax.scatter(data[features], data[labels], color='b',
-            label=u'%s: $y = x + \epsilon$' % "真实值")
-    else:
-        ax.scatter(data[features], data[labels], color='b',
-            label=u'%s: $y = x + \epsilon$' % "真实值".decode("utf-8"))
-    # 画线图，用红色虚线表示95%置信区间
-    # 在Python3中，str不需要decode
-    if sys.version_info[0] == 3:
-        ax.plot(data[features], preUp, "r--", label=u'%s' % "95%置信区间")
-        ax.plot(data[features], re.predict(data[features]), color='r',
-            label=u'%s: $y = %.3fx$'\
-            % ("预测值", re.params[features]))
-    else:
-        ax.plot(data[features], preUp, "r--", label=u'%s' % "95%置信区间".decode("utf-8"))
-        ax.plot(data[features], re.predict(data[features]), color='r',
-            label=u'%s: $y = %.3fx$'\
-            % ("预测值".decode("utf-8"), re.params[features]))
-    ax.plot(data[features], preLow, "r--")
+    # 原数据
+    ax.scatter(data[features], data[labels],
+               color='r',
+               label='%s: $y = x + \epsilon$' % "真实值")
+    # 上界和下界
+    ax.plot(data[features], preUp, "c--", label=u"%s" % "95%置信区间(上)")
+    ax.plot(data[features], preLow, "m--", label=u"%s" % "95%置信区间(下)")
+    # 预测值
+    ax.plot(data[features], re.predict(data[features]), 
+            color='y', 
+            label='%s: $y = %.3fx$' % ("预测值", re.params[features]))
     legend = plt.legend(shadow=True)
     legend.get_frame().set_facecolor('#6F93AE')
     plt.show()
@@ -83,7 +81,19 @@ def visualizeModel(re, data, features, labels):
 
 def trainModel(X, Y):
     """
-    训练模型
+    最小二乘法训练模型
+    
+    Paramters
+    ---------
+    X : pd.DataFrame
+        training data
+    Y : pd.DataFrame or pd.Series
+        training data's labels
+        
+    Returns
+    -------
+    re : model
+        OLS fitted model
     """
     model = sm.OLS(Y, X)
     re = model.fit()
@@ -101,18 +111,15 @@ def linearModel(data):
     features = ["x"]
     labels = ["y"]
     Y = data[labels]
-    # 加入常量变量
+    
+    print('{:*^65}'.format('添加常数项，打印模型参数'))
     X = sm.add_constant(data[features])
-    # 构建模型
-    re = trainModel(X, Y)
-    # 分析模型效果
-    modelSummary(re)
-    # const并不显著，去掉这个常量变量
+    re = trainModel(X, Y) # 特征维度是2维
+    print(modelSummary(re))
+    print('{:*^65}'.format('不添加常数项，打印模型参数'))
     resNew = trainModel(data[features], Y)
-    # 输出新模型的分析结果
     print(resNew.summary())
-    # 将模型结果可视化
-    visualizeModel(resNew, data, features, labels)
+    visualizeModel(resNew, data, features, labels, title="(不添加常数项)")
 
 
 def readData(path):
