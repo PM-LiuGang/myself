94b16f1868703601c03ad1dda73a41ccc2535034
PM.LiuGang <317121415@qq.com>
2019-08-17 20:37:29 +0800

    learn & review

--- qqmail2download.py
+++ qqmail2download.py
@@ -7,11 +7,13 @@
 
 __auother__ = ['PM_LiuGang']
 
+
 import time
 import sys
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException
 
 wb = webdriver.Chrome()
 wb.maximize_window()
@@ -26,66 +28,90 @@ time.sleep(2)
 email_link = wb.find_element_by_xpath('//*[@id="1"]/h3/a[1]')
 email_link.click()
 
-num=wb.window_handles
+num = wb.window_handles
 wb.switch_to_window(num[1])
-wb.switch_to.frame('login_frame') #k
+wb.switch_to.frame('login_frame')  # k
 
-time.sleep(1)
-user = wb.find_element_by_xpath('//*[@id="u"]')
-user.send_keys('317121415@qq.com')
+try:
+    time.sleep(2)
+    user = wb.find_element_by_xpath('//*[@id="u"]')
+    user.send_keys('317121415@qq.com')
 
-time.sleep(1)
-password = wb.find_element_by_xpath('//*[@id="p"]')
-password.send_keys('lg@123123')
+    time.sleep(2)
+    password = wb.find_element_by_xpath('//*[@id="p"]')
+    password.send_keys('lg@123123')
 
-time.sleep(2)
-login_button = wb.find_element_by_xpath('//*[@id="login_button"]')
-login_button.click()
+    time.sleep(2)
+    login_button = wb.find_element_by_xpath('//*[@id="login_button"]')
+    login_button.click()
+
+except ElementNotInteractableException:
+    quick_login = wb.find_element_by_xpath('//*[@id="switcher_plogin"]')
+    quick_login.click()
+
+    time.sleep(2)
+    user = wb.find_element_by_xpath('//*[@id="u"]')
+    user.send_keys('317121415@qq.com')
+
+    time.sleep(2)
+    password = wb.find_element_by_xpath('//*[@id="p"]')
+    password.send_keys('lg@123123')
+
+    time.sleep(2)
+    login_button = wb.find_element_by_xpath('//*[@id="login_button"]')
+    login_button.click()
 
 try:
     time.sleep(2)
     add_password = wb.find_element_by_xpath('//*[@id="pp"]')
     add_login = wb.find_element_by_xpath('//*[@id="btlogin"]')
-    
+
     time.sleep(2)
     add_password.send_keys('5262894aaa')
     add_login.click()
-    
+
     time.sleep(2)
     mh_seach = wb.find_element_by_xpath('//*[@id="subject"]')
     mh_seach.send_keys('didifapiao')
     mh_seach.send_keys(Keys.ENTER)
-    
+
     time.sleep(2)
-    wb.switch_to_frame('mainFrame') #k
-    mail_content = wb.find_element_by_xpath('//*[@id="div_showMonday"]/table/tbody/tr/td[3]/table/tbody/tr[1]/td[1]/nobr/span')
+    wb.switch_to_frame('mainFrame')  # k
+    mail_content = wb.find_element_by_xpath(
+        '//*[@id="div_showMonday"]/table/tbody/tr/td[3]/table/tbody/tr[1]/td[1]/nobr/span')
     mail_content.click()
-    
+
     time.sleep(2)
 #    只下载第一个发票，不会下载行程单
 #    mail_fujian = wb.find_element_by_xpath('//*[@id="attachment"]/div[2]/div[3]/div[2]/div/a[2]')
 #    mail_fujian.click()
-    
-    mail_all_download = wb.find_element_by_xpath('//*[@id="attachment"]/div[2]/div[1]/a[1]')
-    mail_all_download.click() # 有个弊端会把行程单和发票一起打包一起下载，但是会下载全部发票和行程单
+
+    mail_all_download = wb.find_element_by_xpath(
+        '//*[@id="attachment"]/div[2]/div[1]/a[1]')
+    mail_all_download.click()
+
 except NoSuchElementException:
+    time.sleep(2)
     mh_seach = wb.find_element_by_xpath('//*[@id="subject"]')
     mh_seach.send_keys('发票')
     mh_seach.send_keys(Keys.ENTER)
-    
+
     time.sleep(2)
-    wb.switch_to_frame('mainFrame') #k
-    mail_content = wb.find_element_by_xpath('//*[@id="div_showMonday"]/table/tbody/tr/td[3]/table/tbody/tr[1]/td[1]/nobr/span')
+    wb.switch_to_frame('mainFrame')  # k
+    mail_content = wb.find_element_by_xpath(
+        '//*[@id="div_showMonday"]/table/tbody/tr/td[3]/table/tbody/tr[1]/td[1]/nobr/span')
     mail_content.click()
-    
+
     time.sleep(2)
 #    mail_fujian = wb.find_element_by_xpath('//*[@id="attachment"]/div[2]/div[3]/div[2]/div/a[2]')
 #    mail_fujian.click()
-    mail_all_download = wb.find_element_by_xpath('//*[@id="attachment"]/div[2]/div[1]/a[1]')
+    mail_all_download = wb.find_element_by_xpath(
+        '//*[@id="attachment"]/div[2]/div[1]/a[1]')
     mail_all_download.click()
+
 finally:
-    time.sleep(2)
+    time.sleep(10)
     wb.close()
     wb.switch_to_window(num[0])
     wb.close()
-    sys.exit()
+#    sys.exit()
--- xlsx2csv.py
+++ xlsx2csv.py
@@ -9,17 +9,22 @@ import pandas as pd
 import os
 import xlrd
 
-path = r'C:\Users\admin\Desktop'
-filename = 'N4接口测试用例 - 副本.xlsx'
+path = r'D:\360MoveData\Users\86138\Desktop'
+filename = 'zxl1.xlsx'
 #replace_dict = {'wu' : 'None', '无' : 'None'}
 data = xlrd.open_workbook(os.path.join(path, filename))
 sheet_names = data.sheet_names()
 
-for sheet in sheet_names[3 : -1]: 
-    df = pd.read_excel(os.path.join(path, filename), sheet_name=sheet, header=None)
-    df1 = df[1]
-#    df1.replace(replace_dict, inplace=True)
-    df1.fillna('Null', inplace=True)
-    df1_str = ','.join(map(str, df1.to_list()))
-    with open(os.path.join(path, sheet+'.csv'), 'w') as f:
-        f.write(df1_str)
+for sheet in sheet_names[4 : ]: 
+    df = pd.read_excel(os.path.join(path, filename), 
+                       sheet_name=sheet, 
+                       header=None,
+                       skiprows=1,
+                       encoding='gbk')
+    for i in range(1,df.shape[1] + 1,2):
+        df1 = df[i]
+        df1.fillna('Null', inplace=True)
+        df1_str = '|'.join(map(str, df1.to_list()))
+        with open(os.path.join(path, sheet+ '_' + str(i) + '.csv'), 'w', encoding='utf8') as f:
+            f.write(df1_str)
+    
