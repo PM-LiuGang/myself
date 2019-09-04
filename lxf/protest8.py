030589024d4d50503fffe06341171efedabf4087
PM.LiuGang <317121415@qq.com>
2019-09-04 10:30:56 +0800

    add

---
+++ lxf/protest8.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 17:28:20 2019
+@author: PM.LiuGang
+"""
+
+print('hello world!~')
---
+++ lxf/protest9.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 17:41:35 2019
+@author: PM.LiuGang
+"""
+
+#protest9.py
+a = input()
+a = a.split(' ')
+a[0] = str(int(a[0]) + 1)
+print(' '.join(a))
