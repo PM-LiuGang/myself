e7aad5a1a7e2028992a9beaace243ca87fddb330
PM-LiuGang <317121415@qq.com>
2019-09-05 15:27:01 +0800

    add

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -14,17 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-#  from django.urls import path
 
 from django.conf.urls import url
 from django.contrib import admin
 from sign import views
-
-'''
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-'''
 
 urlpatterns = [
     url(r'^$', views.index),
